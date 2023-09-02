ff070afd2739be1a5c96f97eb08da3af0ad7573b
wamba briand <b.wambafoukmeniok@reply.it>
2023-09-02 20:19:35 +0200

    desattivation

--- corsodao.py
+++ corsodao.py
@@ -129,6 +129,7 @@ def getCorsoById(id, idUser, ruolo):
 
         media = getMediaVoto(id, idUser)
         data['media'] = media
+        res['corso'] = data
 
         if ruolo == "CLIENTE":
             userCorso = getUserCorso(idUser, id)
